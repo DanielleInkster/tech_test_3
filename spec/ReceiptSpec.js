c7daedb951ade4a19d55dae3b9494ba37176b31a
DanielleInkster <danielle.inkster@gmail.com>
2019-11-22 16:41:36 +0000

    Setup basic order function

--- spec/ReceiptSpec.js
+++ spec/ReceiptSpec.js
@@ -10,8 +10,15 @@ beforeEach(function() {
   });
 
   it('initializes with an empty order array', () => {
-    expect(receipt.order).toEqual([])
+    expect(receipt.customer_order).toEqual([])
   });
+
+  it('records an order', () => {
+    receipt.order(2, "Cafe Latte")
+    expect(receipt.customer_order[0]).toEqual('2 x Cafe Latte')
+  });
+
+
 
 });
   
